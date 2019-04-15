16778423d33c771d5f266852527694ad2a7bebd1
Fabian Gutierrez <fega.hg@gmail.com>
2019-04-15 07:36:15 -0500

    Added readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Google cloud function image Store
+
+This is a basic boilerplate for a Node.js service that can store images in google cloud function from image-64 strings.
+
+## Features
+
+- Uses Sharp (AKA, very fast);
+- Miniature generation support
+- Serverless Framework support
+- Max Sizing support
+- No more Service Scale pains!
+
+## Usage
+
+1. Clone this repository
+2. Change the config.js File (Check the comments in that file)
+3. Change the serverless.yml config file
+4. Deploy an enjoy
+
+## License
+
+MIT
--- config.js
+++ config.js
@@ -1,25 +1,41 @@
 module.exports = {
+  // allowed file format extensions
   allowedExtensions: ['png', 'jpg', 'gif', 'webp', 'gif', 'svg'],
+
+  // google cloud bucket name
   bucket: 'bucket-name',
+
+  // Resize options (Only when image size is larger)
   resize: {
     width: 800,
     height: 800,
     fit: 'inside',
   },
+
+  // Use this to save it in a different format (set as false or comment to disable it)
   outputFormat: 'jpeg',
+
+  // Sharp Output Options http://sharp.pixelplumbing.com/en/stable/api-output/#jpeg
   outputOptions: {
     quality: 80,
     progressive: true,
   },
   /**
-   * Set the name
+   * Set the save file name
    * @param {string} or original name
    * @param {string} ext file extension
    * @param {string} tt timestamp
    */
   name: (or, ext, tt) => `${or}-${tt}.${ext}`,
+  /**
+   * Set the save path
+   * @param {string} or original name
+   * @param {string} ext file extension
+   * @param {string} tt timestamp
+   */
   path: (or, ext, tt) => '/images/',
 
+  // Array of miniatures to be generated (Optional) 
   miniatures: [{
     resize: {
       width: 100,
